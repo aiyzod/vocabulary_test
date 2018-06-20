b7ce56b830c77c948a4cd797350251dffe40abdf
陳彥甫 <qqabp870901@gmail.com>
2018-06-21 02:11:51 +0800

    Record update

--- app/src/main/java/com/example/asus/learningenglish/RecordActivity.java
+++ app/src/main/java/com/example/asus/learningenglish/RecordActivity.java
@@ -1,7 +1,9 @@
 package com.example.asus.learningenglish;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.Objects;
 
@@ -12,5 +14,14 @@ public class RecordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        TextView record_title = findViewById(R.id.record_title);
+        record_title.setTypeface(Typeface.createFromAsset(getAssets(), "setofont.ttf"));
+        TextView record_score = findViewById(R.id.record_score);
+        record_score.setTypeface(Typeface.createFromAsset(getAssets(), "setofont.ttf"));
+        TextView record_q_index = findViewById(R.id.record_q_index);
+        record_q_index.setTypeface(Typeface.createFromAsset(getAssets(), "setofont.ttf"));
+        TextView record_player = findViewById(R.id.record_player);
+        record_player.setTypeface(Typeface.createFromAsset(getAssets(), "setofont.ttf"));
     }
 }
