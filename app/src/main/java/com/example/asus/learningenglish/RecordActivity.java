a71aa1728314ae60d96c7a49ab268c1034c0e35f
楊怡君 <delete135430@gmail.com>
2018-06-21 04:20:22 +0800

    Record update

--- app/src/main/java/com/example/asus/learningenglish/RecordActivity.java
+++ app/src/main/java/com/example/asus/learningenglish/RecordActivity.java
@@ -1,13 +1,18 @@
 package com.example.asus.learningenglish;
 
+import android.database.Cursor;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Objects;
 
 public class RecordActivity extends AppCompatActivity {
+
+    TextView record_score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,31 @@ public class RecordActivity extends AppCompatActivity {
 
         TextView record_title = findViewById(R.id.record_title);
         record_title.setTypeface(Typeface.createFromAsset(getAssets(), "setofont.ttf"));
-        TextView record_score = findViewById(R.id.record_score);
+        record_score = findViewById(R.id.record_score);
         record_score.setTypeface(Typeface.createFromAsset(getAssets(), "setofont.ttf"));
         TextView record_q_index = findViewById(R.id.record_q_index);
         record_q_index.setTypeface(Typeface.createFromAsset(getAssets(), "setofont.ttf"));
         TextView record_player = findViewById(R.id.record_player);
         record_player.setTypeface(Typeface.createFromAsset(getAssets(), "setofont.ttf"));
+        Button btn_reset = findViewById(R.id.btn_reset);
+
+        Cursor cursor = MainActivity.db.rawQuery("select * from " + DBOpenHelper.DATABASE_TABLE ,null);
+        cursor.moveToFirst();
+        record_score.setText(cursor.getString(cursor.getColumnIndex("chinese")));
+        record_player.setText(String.format("by %s", cursor.getString(cursor.getColumnIndex("english"))));
+        cursor.close();
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.db.execSQL("update " + DBOpenHelper.DATABASE_TABLE + " set chinese='0' where id='0'");
+                record_score.setText("0");
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
